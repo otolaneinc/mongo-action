4e43d96adcf9ead060a287ec374cf3719cd605bc
Souri Guha <souriguha@gmail.com>
2024-03-05 20:25:44 +0530

    Remove sudo

--- dist/index.js
+++ dist/index.js
@@ -2831,7 +2831,7 @@ const { exec } = __nccwpck_require__(81);
 const image_version = core.getInput("image_version");
 const port = core.getInput("port");
 
-const command = `sudo docker run -d -p ${port}:27017 mongo:${image_version}`;
+const command = `docker run -d -p ${port}:27017 mongo:${image_version}`;
 
 console.log("Executing the following command: ");
 console.log(command);
